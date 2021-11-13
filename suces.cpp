1f295b51d7fb2915b799b5ecd7d2a57ac532b629
pebalmeida <93488924+pebalmeida@users.noreply.github.com>
2021-11-13 17:18:06 -0300

    Add files via upload

---
+++ suces.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int n, a, s;
+	printf("Digite um numero:");
+	scanf("%d", &n);
+	a = n - 1;
+	s = n + 1;
+	printf("O seu antecessor vale %d e o seu sucessor vale %d é", a, s);
+}
